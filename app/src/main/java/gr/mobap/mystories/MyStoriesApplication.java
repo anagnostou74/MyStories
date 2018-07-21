bff8107f762c8e117e148da762ba6422c1b23d0d
Costas Anagnostou <anagnostou74@gmail.com>
2018-07-21 17:08:39 +0300

    fix: Application now initialize Firebase db

--- app/src/main/java/gr/mobap/mystories/MyStoriesApplication.java
+++ app/src/main/java/gr/mobap/mystories/MyStoriesApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.FirebaseDatabase;
 import com.twitter.sdk.android.core.DefaultLogger;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -13,13 +14,16 @@ import com.twitter.sdk.android.tweetui.TweetUi;
 import static gr.mobap.mystories.BuildConfig.CONSUMER_KEY;
 import static gr.mobap.mystories.BuildConfig.CONSUMER_SECRET;
 
-class MyStoriesApplication extends Application {
+public class MyStoriesApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         // Obtain the FirebaseAnalytics instance.
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        // initialize firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        database.setPersistenceEnabled(true);
 
         //Twitter sdk initialization
         TwitterConfig config = new TwitterConfig.Builder(this)
